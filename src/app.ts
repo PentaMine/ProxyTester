eefc7765b6e0e5b8b50b0a2d51c0d59f085b5e61
PentaMine <Sale354.yt@gmail.com>
2023-10-10 22:09:07 +0200

    dotenv port configuration

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,12 @@
 import express, {Express, Request, Response} from 'express';
 import * as https from "https";
 import * as fs from "fs";
+import {configDotenv} from "dotenv";
+
+configDotenv()
+
+const httpPort = process.env.HTTP_PORT
+const httpsPort = process.env.HTTPS_PORT
 
 const server: Express = express();
 const httpsServer = https.createServer(
@@ -15,11 +21,10 @@ server.get("/", (req: Request, res: Response) => {
     res.json({secure: req.secure, headers: req.headers})
 })
 
-
-httpsServer.listen(443, () => {
-    console.log(`HTTPS server up at https://localhost:${443}`);
+server.listen(httpPort, () => {
+    console.log(`HTTP server up at http://localhost:${httpPort}`);
 });
 
-server.listen(80, () => {
-    console.log(`HTTP server up at http://localhost:${80}`);
+httpsServer.listen(httpsPort, () => {
+    console.log(`HTTPS server up at https://localhost:${httpsPort}`);
 });
